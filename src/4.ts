3d37283d39d95b583ade4a3e7daa7a38fa641965
mariiapelypyshyn <maria.peleshko97@gmail.com>
2023-12-10 12:02:51 +0200

    homework 3

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,64 @@
+// Клас Key
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+// Клас Person
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+// Абстрактний клас House
+abstract class House {
+  protected door: boolean;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.door = false;
+    this.key = key;
+  }
+
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log('Person entered the house.');
+    } else {
+      console.log('The door is closed.');
+    }
+  }
+}
+
+// Клас MyHouse
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log('The door is opened.');
+    } else {
+      console.log('Invalid key. The door remains closed.');
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
